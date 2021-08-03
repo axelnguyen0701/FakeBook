661ebd99a95646cb86b5b79f430a5c0f1192f167
Hieu Nguyen <axelnguyen0701@gmail.com>
2021-08-03 15:07:14 -0600

    add utilities

--- src/App.js
+++ src/App.js
@@ -1,58 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import { AddPostForm } from "./features/post/AddPostForm";
 
-function App() {
+import { PostsList } from "./features/post/PostsList";
+import { SinglePostPage } from "./features/post/SinglePostPage";
+import { UserPage } from "./features/users/UserPage";
+import { UsersList } from "./features/users/UsersList";
+import { NavBar } from "./features/post/NavBar";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Container className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <>
+                <AddPostForm />
+                <PostsList />
+              </>
+            )}
+          />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
+
+          <Redirect to="/" />
+        </Switch>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
