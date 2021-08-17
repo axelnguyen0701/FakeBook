ab5391273d484cacb952c925abca870d0d3bad5d
Hieu Nguyen <axelnguyen0701@gmail.com>
2021-08-16 22:05:12 -0600

    UI change

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,26 +1,44 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { selectUserById } from "../users/usersSlice";
 import { selectAllPosts } from "../post/postsSlice";
+import { PostExcerpt } from "../post/PostsList";
+
+//Material UI
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
 export const UserPage = ({ match }) => {
   const { userId } = match.params;
   const user = useSelector((state) => selectUserById(state, userId));
   const postsForUser = useSelector((state) => {
     const allPosts = selectAllPosts(state);
-    return allPosts.filter((post) => post.author === userId);
+    return allPosts
+      .filter((post) => post.author === userId)
+      .sort((a, b) => b.date.localeCompare(a.date));
   });
 
-  const postTitles = postsForUser.map((post) => (
-    <li key={post.id}>
-      <Link to={`/posts/${post.id}`}>{post.content}</Link>
-    </li>
+  const renderedPosts = postsForUser.map((post) => (
+    <Grid item key={post.id} xs={12}>
+      <PostExcerpt post={post} user={user} />
+    </Grid>
   ));
 
   return (
     <section>
-      <h2>{user.name}</h2>
-      <ul>{postTitles}</ul>
+      <Typography variant="h3" component="h2">
+        {user.username}'s profile
+      </Typography>
+      <Grid
+        container
+        spacing={3}
+        justifyContent="center"
+        style={{ marginTop: "1rem" }}
+      >
+        <Grid container item spacing={3}>
+          {renderedPosts}
+        </Grid>
+      </Grid>
     </section>
   );
 };
