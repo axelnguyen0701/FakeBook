45663dd2fd6b51ea6b004b70ddba52afea1d9549
Hieu Nguyen <axelnguyen0701@gmail.com>
2021-08-16 22:03:14 -0600

    Don't use global settings for api call

--- src/api/server.js
+++ src/api/server.js
@@ -2,7 +2,4 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: process.env.REACT_APP_SERVER,
-  headers: {
-    authorization: "bearer " + localStorage.getItem("user_token"),
-  },
 });
