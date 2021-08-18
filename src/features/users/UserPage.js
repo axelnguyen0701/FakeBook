7a4964ae1f1ed4db248a8367dab084a5697e3672
Hieu Nguyen <axelnguyen0701@gmail.com>
2021-08-18 14:37:46 -0600

    Add user card

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -2,11 +2,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectUserById } from "../users/usersSlice";
 import { selectAllPosts } from "../post/postsSlice";
-import { PostExcerpt } from "../post/PostsList";
-
+import { PostExcerpt } from "../post/PostExcerpt";
+import { AddPostForm } from "../post/AddPostForm";
 //Material UI
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+import { Container } from "@material-ui/core";
+import UserCard from "./UserCard";
 
 export const UserPage = ({ match }) => {
   const { userId } = match.params;
@@ -25,20 +26,29 @@ export const UserPage = ({ match }) => {
   ));
 
   return (
-    <section>
-      <Typography variant="h3" component="h2">
-        {user.username}'s profile
-      </Typography>
+    <Container>
       <Grid
         container
         spacing={3}
         justifyContent="center"
         style={{ marginTop: "1rem" }}
       >
-        <Grid container item spacing={3}>
+        <Grid
+          container
+          item
+          xs={3}
+          alignItems="flex-start"
+          justifyContent="center"
+        >
+          <UserCard user={user} />
+        </Grid>
+        <Grid container item spacing={3} xs={6}>
+          <Grid item xs={12}>
+            <AddPostForm />
+          </Grid>
           {renderedPosts}
         </Grid>
       </Grid>
-    </section>
+    </Container>
   );
 };
